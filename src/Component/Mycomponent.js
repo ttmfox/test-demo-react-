d3341b3373214f9b3de366b5d1ea9c8a57849b96
ttmfox <thien0972089317@gmail.com>
2024-11-14 23:24:29 +0700

    video 20

--- src/Component/Mycomponent.js
+++ src/Component/Mycomponent.js
@@ -1,50 +1,13 @@
 import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
+import UseInfor from "./UseInfor";
 
 class Mycomponent extends React.Component {
-
-    state = {
-        Name: 'Nguyen Quang Thien',
-        address: 'Hai Duong',
-        age: 18
-    };
-    HandleClick(event) {
-        console.log(">> click me button")
-        // console.log(event)
-        // console.log("my name is", this.state.Name)
-        // console.log("random", Math.floor((Math.random() * 100) + 1))
-        this.setState({
-            Name: "Eric",
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-
-
-    HandleOnmouseOver(event) {
-        // console.log(event)
-    }
-    HandleOnChangeInput = (event) => {
-        this.setState({
-            Name: event.target.value
-        })
-    }
-
-    HandleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-    }
-
     render() {
         return (
             <div>
-                My name is {this.state.Name} and Im {this.state.age}
-                <form onSubmit={(event) => this.HandleOnSubmit(event)}>
-                    <input type="text"
-                        onChange={(event) => { this.HandleOnChangeInput(event) }}
-                    />
-                    <button>submit</button>
 
-                </form>
+                <UseInfor />
             </div>
         );
     }
---
+++ src/Component/UseInfor.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class UseInfor extends React.Component {
+    state = {
+        Name: 'Nguyen Quang Thien',
+        address: 'Hai Duong',
+        age: 18
+    };
+    HandleOnChangeInput = (event) => {
+        this.setState({
+            Name: event.target.value
+        })
+    }
+    HandleOnChangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    HandleOnSubmit = (event) => {
+        event.preventDefault(); // ngăn chặn load lại trang 
+        console.log(this.state)
+    }
+    render() {
+
+        return (
+            <div>
+                My name is {this.state.Name} and Im {this.state.age}
+                <form onSubmit={(event) => this.HandleOnSubmit(event)}>
+                    <lable>Your Name</lable>
+                    <input
+                        value={this.state.Name}
+                        type="text"
+                        onChange={(event) => { this.HandleOnChangeInput(event) }}
+                    />
+
+                    <lable>Your Age</lable>
+                    <input
+                        value={this.state.age}
+                        type="text"
+                        onChange={(event) => { this.HandleOnChangeAge(event) }}
+                    />
+                    <button>submit</button>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UseInfor;
