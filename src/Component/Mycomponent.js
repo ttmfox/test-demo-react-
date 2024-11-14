d325a5b157ff1041dd8679d4f3796b0d8f56d4d5
ttmfox <thien0972089317@gmail.com>
2024-11-14 17:16:03 +0700

    video16

---
+++ src/Component/Mycomponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Mycomponent extends React.Component {
+    render() {
+        return (
+            <div>
+                My firt component
+            </div>
+        )
+    }
+}
+export default Mycomponent;
